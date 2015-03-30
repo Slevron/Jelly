02a4e4519725188695a4e0f60039517a83dfe73e
Sonic Fag <m.gammaitoni@student.isartdigital.com>
2015-03-30 14:30:12 +0200

    Rajout de state

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,46 @@
-var game = new Phaser.Game(1280, 700, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+window.onload = function() 
+{
+	var game = new Phaser.Game(1280, 700, Phaser.AUTO, '');
 
-function preload() {
-}
+	var run = {
+        //PRELOAD
+        preload: function(){
+          this.game.load.image('bg','../assets/bg2.png');
+        
+        },
+        create: function(){
+            this.game.bg=this.game.add.sprite(0,0,'bg');
+          
+           
+        },
+        update:function(){
 
-function create() {
-}
+        },    
+    };
 
-function update() {
-}
+
+
+    var menu = {
+        //PRELOAD
+        preload: function(){
+          this.game.load.image('bg','../assets/bg2.png');
+        
+        },
+        create: function(){
+            this.game.bg=this.game.add.sprite(0,0,'bg');
+          
+           
+        },
+        update:function(){
+
+        },    
+    };
+   
+    //------------------STATS-----------------
+    game.state.add('run',run);
+    game.state.add('menu',menu);
+    game.state.start('menu',run);
+
+    //---------------------------------------
+
+};
