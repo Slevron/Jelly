baa707b52e1f1ad451ee61d4759e5bba313620c2
Cutter <s.levron.pro@gmail.com>
2015-03-31 15:06:21 +0200

    Update map.collider

--- src/js/Map.js
+++ src/js/Map.js
@@ -3,7 +3,7 @@ function Map(game){
     console.log(game);
     this.map = game.add.tilemap('level1');
     this.map.addTilesetImage('tiles-1');
-    this.map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+    this.map.setCollisionByExclusion([ 0 ]);
     this.layer = this.map.createLayer('Tile Layer 1');
     this.layer.resizeWorld();
 };
