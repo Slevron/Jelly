21cc9f03decade98c8c9eb14ce303bf5b628a5b9
Cutter <s.levron.pro@gmail.com>
2015-03-31 16:36:12 +0200

    Update img menu

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -3,10 +3,11 @@ MenuState.prototype =
 {
         //PRELOAD
         preload: function(){
-          this.game.load.image('bg','src/assets/magic.jpg');
+          this.game.load.image('play','src/assets/play.png');
         },
         create: function(){
-            this.game.bg=this.game.add.button(0,0,"bg",this.goRun);
+            this.game.bg=this.game.add.button(1280/2,300,"play",this.goRun);
+            this.game.bg.anchor.setTo(0.5,0.5);
         },
         update:function(){
         },
