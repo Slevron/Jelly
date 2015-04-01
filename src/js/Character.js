b7496c26700a6ffd4bb9e56274d6f26e55627a04
Sonic Fag <m.gammaitoni@student.isartdigital.com>
2015-04-01 18:36:10 +0200

    animation ok

--- src/js/Character.js
+++ src/js/Character.js
@@ -9,8 +9,12 @@ function Character(game){
     this.sprite.body.bounce.y = 0;
     this.sprite.body.collideWorldBounds = true;
     this.sprite.body.setSize(80, 90,4,16);
-    this.sprite.animations.add('left', [1, 2, 3, 4], 10, true);
+    this.sprite.animations.add('walk', [1, 2, 3, 4], 10, true);
     this.sprite.animations.add('jump', [0], 10, true);
+    this.sprite.animations.add('idle',[5],10,true);
+    this.sprite.animations.add('hurt',[7,8,9,10,11,6],20,true);
+
+    this.sprite.frame=5;
     //this.sprite.animations.add('turn', [4], 20, true);
     //this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
     game.camera.follow(this.sprite, Phaser.Camera.FOLLOW_PLATFORMER);
@@ -34,78 +38,55 @@ Character.prototype.update = function(){
         if(this.timeSinceHit >= this.invicibleTime){
             this.hitable = true;
             this.canInput=true;
-
         }
     }
     for (var i = 0; i < this.shoots.length; i++) {
        this.shoots[i].update();
     };
-    if(this.canInput)
+    
     this.move();
 
 };
 Character.prototype.move =function(){
-    this.sprite.body.velocity.x=0;
     
-    if (this.cursors.left.isDown){
-        this.sprite.body.velocity.x = -250;
-        if (this.facing != 'left')
-        {
-            this.sprite.animations.play('left');
-            this.facing = 'left';
-            this.sprite.scale={x:-this.scaleBase,y:this.scaleBase};    
-        }
-        
-    }
-    else if (this.cursors.right.isDown)
+     if(this.canInput)
     {
-        this.sprite.body.velocity.x = 250;
-        if (this.facing != 'right')
-        {
-            this.sprite.animations.play('left');
-            this.facing = 'right';
+        this.sprite.body.velocity.x=0;
+         if (this.cursors.left.isDown){
+
+        this.state="walk";
+        this.sprite.body.velocity.x = -250;
+        this.sprite.scale={x:-this.scaleBase,y:this.scaleBase};
+    }
+        if (this.cursors.right.isDown){
+            this.state="walk";
+            this.sprite.body.velocity.x = 250;
             this.sprite.scale={x:this.scaleBase,y:this.scaleBase};
         }
-    }
-
-    if (this.jumpButton.isDown && this.sprite.body.onFloor() && this.refGame.time.now > this.jumpTimer)
-    {
-        this.sprite.body.velocity.y = -800;
-        this.jumpTimer = this.refGame.time.now + 750;   
-    }
-    else if(this.sprite.body.onFloor())
-    {
-        this.jumpTimer=0;
-    }
-    if(this.sprite.body.onFloor()&&!this.cursors.right.isDown&&!this.cursors.left.isDown)
-    {
-        if(this.sprite.body.onFloor()){
-            if (this.facing != 'idle')
-            {
-                this.sprite.animations.stop();
-                if (this.facing == 'left')
-                {
-                    this.sprite.frame = 5;
-                }
-                else
-                {   
-                   this.sprite.frame = 5;
-                }   
-                this.facing = 'idle';
-            }
-
+        if (this.jumpButton.isDown && this.sprite.body.onFloor() && this.refGame.time.now > this.jumpTimer)
+        {
+            this.state="isJumping";
+            this.sprite.body.velocity.y = -800;
+            this.jumpTimer = this.refGame.time.now + 750;   
+        }
+        else if(this.sprite.body.onFloor()&& this.state!="walk")
+        {
+            this.state="idle";
+            this.jumpTimer=0;
+        }
+        else if(!this.sprite.body.onFloor())
+        {
+            this.state="jump"; 
         }
     }
-    else if(!this.sprite.body.onFloor())
-    {
-       this.sprite.animations.play('jump');
-    }
-    else if(this.sprite.body.onFloor&&(this.cursors.right.isDown||this.cursors.left.isDown)){
-        this.sprite.animations.play("left")
-    }
+   
+    
+        
+ 
     //---------------SHOOT.
-    if(this.shootButton.isDown&& this.canshoot)
+    if(this.shootButton.isDown&& this.canshoot&&this.state!="hurt")
     {
+       
         this.launchShoot();
         
     }        
@@ -117,6 +98,9 @@ Character.prototype.move =function(){
     {
         this.canshoot=true;
     }
+    console.log(this.state);
+    
+    this.checkStateForAnim(this.state);
 };
 Character.prototype.launchShoot = function(){
    global.actionEtoile+=1;
@@ -125,6 +109,7 @@ Character.prototype.launchShoot = function(){
    this.canInput=true;
 };
 Character.prototype.takeDamage = function(damage){
+     this.state="hurt";
     this.health -= damage;
     this.hitable = false;
     this.timeSinceHit = 0; 
@@ -138,4 +123,9 @@ Character.prototype.takeDamage = function(damage){
    game.add.tween(this.sprite.scale).to({x:newScale.x,y:newScale.y}, 1000, Phaser.Easing.Cubic.Out,true);
    this.sprite.body.offset.x *= newScale.x;
    this.sprite.body.offset.y = 16 * newScale.y;
+};
+Character.prototype.checkStateForAnim= function(state){
+  if(this.sprite.frame<7)
+    this.sprite.animations.play(state);
+
 };
