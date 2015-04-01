b250e3f9ebaf0c6c9d132cbbebd4761aad479ab5
Sonic Fag <m.gammaitoni@student.isartdigital.com>
2015-04-01 12:06:32 +0200

    new take damage

--- src/js/Character.js
+++ src/js/Character.js
@@ -103,25 +103,25 @@ Character.prototype.move =function(){
 };
 Character.prototype.launchShoot = function(){
    
-   var bodyOri = {x:this.sprite.body.x,y:this.sprite.body.y,width:this.sprite.body.width,height:this.sprite.body.height};
-   var spriteOri = {x:this.sprite.x,y:this.sprite.y,height:this.sprite.height,width:this.sprite.width};
+   
+   this.shoots.push(new Shoot(this.refGame,this.sprite.x,this.sprite.y,this.sprite.scale.x));
+   this.takeDamage(0.04);
+};
+Character.prototype.takeDamage = function(damage){
+    console.log("DAMAVE")
+    this.health -= damage;
+    this.hitable = false;
+    this.timeSinceHit = 0; 
 
    var newScale={x:0,y:0};
    var dir=0;
-   this.shoots.push(new Shoot(this.refGame,this.sprite.x,this.sprite.y,this.sprite.scale.x));
-   this.scaleBase -= this.scaleBase*0.04;
+   this.scaleBase -= this.scaleBase*damage;
    newScale.x=this.scaleBase* (dir= this.sprite.scale.x > 0 ? 1 : -1);
    newScale.y=this.scaleBase* (dir= this.sprite.scale.y > 0 ? 1 : -1); 
 
    game.add.tween(this.sprite.scale).to({x:newScale.x,y:newScale.y}, 1000, Phaser.Easing.Cubic.Out,true);
-   console.log(this.sprite.body)
    this.sprite.body.offset.x *= newScale.x;
    this.sprite.body.offset.y = 16 * newScale.y;
-
-};
-Character.prototype.takeDamage = function(damage){
-    this.health -= damage;
-    this.hitable = false;
-    this.timeSinceHit = 0;
+   console.log(this.sprite.scale)
 
 };
