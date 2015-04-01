f2bc1be67c5c45f7200eb71df22a323ccda7f385
Sonic Fag <m.gammaitoni@student.isartdigital.com>
2015-04-01 11:06:57 +0200

    new good scale

--- src/js/Character.js
+++ src/js/Character.js
@@ -7,11 +7,12 @@ function Character(game){
     this.facing = 'left'; // la direction du regard du player
     this.sprite.body.bounce.y = 0;
     this.sprite.body.collideWorldBounds = true;
-    this.sprite.body.setSize(80, 90, 4, 16);
+    this.sprite.body.setSize(80, 90,4,16);
     this.sprite.animations.add('left', [0, 1, 2, 3], 10, true);
     //this.sprite.animations.add('turn', [4], 20, true);
     //this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
-    game.camera.follow(this.sprite);
+    game.camera.follow(this.sprite, Phaser.Camera.FOLLOW_PLATFORMER);
+    
     this.cursors = game.input.keyboard.createCursorKeys();
     this.jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     this.shootButton=  game.input.keyboard.addKey(Phaser.Keyboard.A);
@@ -21,6 +22,7 @@ function Character(game){
     this.timeSinceHit = 0;
     this.invicibleTime = 1;
     this.scaleBase=1;
+    console.log(this.sprite);
 };
 Character.prototype.constructor = Character;
 Character.prototype.update = function(){
@@ -39,7 +41,7 @@ Character.prototype.move =function(){
     this.sprite.body.velocity.x=0;
     
     if (this.cursors.left.isDown){
-        this.sprite.body.velocity.x = -150;
+        this.sprite.body.velocity.x = -250;
         if (this.facing != 'left')
         {
             this.sprite.animations.play('left');
@@ -49,7 +51,7 @@ Character.prototype.move =function(){
     }
     else if (this.cursors.right.isDown)
     {
-        this.sprite.body.velocity.x = 150;
+        this.sprite.body.velocity.x = 250;
         if (this.facing != 'right')
         {
             this.sprite.animations.play('left');
@@ -98,13 +100,19 @@ Character.prototype.move =function(){
     }
 };
 Character.prototype.launchShoot = function(){
+   
+   var bodyOri = {x:this.sprite.body.x,y:this.sprite.body.y,width:this.sprite.body.width,height:this.sprite.body.height};
+   var spriteOri = {x:this.sprite.x,y:this.sprite.y,height:this.sprite.height,width:this.sprite.width};
 
-    var dir=0;
+   var newScale={x:0,y:0};
+   var dir=0;
    this.shoots.push(new Shoot(this.refGame,this.sprite.x,this.sprite.y,this.sprite.scale.x));
-   this.scaleBase -= this.scaleBase*0.02;
-   this.sprite.scale.x=this.scaleBase* (dir= this.sprite.scale.x > 0 ? 1 : -1);
-   this.sprite.scale.y=this.scaleBase* (dir= this.sprite.scale.x > 0 ? 1 : -1); 
+   this.scaleBase -= this.scaleBase*0.04;
+   newScale.x=this.scaleBase* (dir= this.sprite.scale.x > 0 ? 1 : -1);
+   newScale.y=this.scaleBase* (dir= this.sprite.scale.y > 0 ? 1 : -1); 
 
-
-
+   game.add.tween(this.sprite.scale).to({x:newScale.x,y:newScale.y}, 1000, Phaser.Easing.Cubic.Out,true);
+   console.log(this.sprite.body)
+   this.sprite.body.offset.x *= newScale.x;
+   this.sprite.body.offset.y = 16 * newScale.y;
 };
