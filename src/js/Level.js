2f289c411cf6ba4d02e2374e34667a406d04f2b0
Cutter <s.levron.pro@gmail.com>
2015-04-01 11:17:55 +0200

    Fix

--- src/js/Level.js
+++ src/js/Level.js
@@ -1,10 +1,17 @@
-var LevelState = function(game) {  }; 
+
+var LevelState = function(game,id) {
+    global.idLevel = id;
+}; 
 LevelState.prototype = 
 {
         //PRELOAD
+        
         preload: function(){
+            console.log(idLevel)
+            this.game.load.json('config'+idLevel+'', 'src/json/config'+idLevel+'.json');
         	this.game.load.image('bg','src/assets/magic.jpg');
-    		this.game.load.tilemap('level1', 'src/json/level3.json', null, Phaser.Tilemap.TILED_JSON);
+    		this.game.load.tilemap('level'+idLevel+'', 'src/json/level'+idLevel+'.json', null, Phaser.Tilemap.TILED_JSON);
+    		this.game.load.tilemap('level1', 'src/json/leveltest.json', null, Phaser.Tilemap.TILED_JSON);
    	 		this.game.load.image('tiles-1', 'src/assets/tiles-1.png');
     		this.game.load.spritesheet('dude', 'src/assets/dude.png', 115, 131);
     		this.game.load.spritesheet('droid', 'src/assets/droid.png', 32, 32);
@@ -14,25 +21,28 @@ LevelState.prototype =
         
         },
         create: function(){
-            //--init des instance d'objet
+            //--init des instance d'objets
             
-            //
+            var phaserJSON = game.cache.getJSON('config'+idLevel+'');
+        
+            game.add.plugin(Phaser.Plugin.Debug);
+            
+            game.stage.backgroundColor = '#FFFFFF';
 
-        	game.add.plugin(Phaser.Plugin.Debug);
-        	
-
-			game.stage.backgroundColor = '#FFFFFF';
-
-			//bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-			//bg.fixedToCamera = true;
-            this.game.map = new Map(game);
+            //bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+            //bg.fixedToCamera = true;
+            this.game.map = new Map(game,idLevel);
 
             game.enemies = game.add.group();
-
-            new Roach(game,100,100,[{x:-150,y:100},{x:150,y:100}]);
-
-			
-            this.game.character = new Character(game);
+            //Rewards
+            global.cacahueteMax = phaserJSON.reward.cacahueteMax;
+            global.timeMax = phaserJSON.reward.timeMax;
+            global.actionMax = phaserJSON.reward.actionMax;
+            //
+            for (var i = 0; i < phaserJSON.roachs.length; i++) {
+                new Roach(game,phaserJSON.roachs[i].x,phaserJSON.roachs[i].y,phaserJSON.roachs[i].waypoints);
+            };
+            this.game.character = new Character(game,phaserJSON.player.x,phaserJSON.player.y);
             game.physics.startSystem(Phaser.Physics.ARCADE);
             game.physics.arcade.gravity.y = 1500;
 
