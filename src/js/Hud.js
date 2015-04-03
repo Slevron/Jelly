710dd9ed4d32a85b963fddd50a1970ef0d53c89c
Real Original Guyot <t.guyot60@laposte.net>
2015-04-03 02:38:16 +0200

    add pause button

--- src/js/Hud.js
+++ src/js/Hud.js
@@ -1,10 +1,20 @@
 function Hud(game){
+	var that = this;
 	this.refGame = game;
 	this.sprites = {coeur:game.add.sprite(50,50,"coeur")};
 	this.sprites.coeur.fixedToCamera = true;
 	this.sprites.coeur.animations.add("good",[0,0,0,0,0,0,0,00,0,0,1,0,1],8,true);
 	this.sprites.coeur.animations.add("bad",[2,2,2,2,2,2,3],12,true);
 	this.sprites.coeur.animations.play("good");
+	this.sprites.pause = game.add.button(1280-150 , 50, 'playButton',function(){
+		console.log("hellolololol")
+		that.refGame.pauseGame = !that.refGame.pauseGame;
+	});
+
+	this.sprites.pause.inputEnabled = true;
+	this.sprites.pause.events.onInputUp.add(function () {this.refGame.paused = true;},this);
+	game.input.onDown.add(function () {if(this.refGame.paused)this.refGame.paused = false;},this);
+
 	this.changeAnim = function (nameObjectToChange, animName){
 		this.sprites[nameObjectToChange].play(animName);
 	}
